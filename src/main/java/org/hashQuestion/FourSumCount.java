45c205753ec8f83973b241afcf2d03c0b3f5c514
pudding <gyh020815@outlook.com>
2025-01-12 23:17:46 +0800

    add:1.12  四数相加2 用哈希表

---
+++ src/main/java/org/hashQuestion/FourSumCount.java
@@ -0,0 +1,30 @@
+package org.hashQuestion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author pudding
+ * @email gyh020815@outlook.com
+ * @Date 2025/1/12 下午10:57
+ */
+/*四数相加 TODO hash经典题目*/
+public class FourSumCount {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int u : nums1) {
+            for (int v : nums2) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : nums3) {
+            for (int v : nums4) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
