e8b8a9515396457128ad58abe1b3913808899253
pudding <gyh020815@outlook.com>
2025-01-19 19:13:33 +0800

    add:1.19 对称二叉树判断 迭代  递归

--- src/main/java/org/tree/simple/IsSymmetric.java
+++ src/main/java/org/tree/simple/IsSymmetric.java
@@ -2,7 +2,6 @@ package org.tree.simple;
 
 import org.tree.TreeNode;
 
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -39,58 +38,61 @@ public class IsSymmetric {
         return true;
     }
 
+
+    /*递归写法 判断是否为对称*/
     public boolean isSymmetric1(TreeNode root) {
-        if (root == null) {
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
             return true;
         }
-        Deque<TreeNode> deque = new LinkedList<>();
-        deque.offer(root);
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            int[] arr = new int[size];
-            for (int i = 0; i < size; i++) {
-                TreeNode node = deque.poll();
-                arr[i] = node.val;
+        if (left == null || right == null) {
+            return false;
+        }
+        /**/
+        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
 
 
+    /*把根节点入队两次 迭代法（迭代只能采用后序遍历）*/
+    public boolean isSymmetric2(TreeNode root) {
+        return check(root, root);
+    }
 
-                if (node.left != null) {
-                    deque.offer(node.left);
-                }
-                if (node.right != null) {
-                    deque.offer(node.right);
-                }
-
+    public boolean check1(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
             }
-            if (!duichen(arr)) {
+            if ((u == null || v == null) || (u.val != v.val)) {
                 return false;
             }
 
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
         }
         return true;
     }
 
-    public boolean duichen(int[] nums) {
-        int len = nums.length,left=0,right=len-1;
-        while (left < right) {
-            if (nums[left] != nums[right]) {
-                return false;
-            }else {
-                left++;
-                right--;
-            }
-        }
-        return true;
-    }
 
 
     public static void main(String[] args) {
         TreeNode lr = new TreeNode(2);
         TreeNode rl = new TreeNode(2);
         TreeNode left = new TreeNode(2);
-        left.right=lr;
+        left.right = lr;
         TreeNode right = new TreeNode(2);
-        right.left=rl;
+        right.left = rl;
         TreeNode root = new TreeNode(1, left, right);
         IsSymmetric isSymmetric = new IsSymmetric();
         System.out.println(isSymmetric.isSymmetric(root));
