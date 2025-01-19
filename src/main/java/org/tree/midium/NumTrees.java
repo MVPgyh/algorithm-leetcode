797c5bdb95934c5ad8099da1c42e7ab83f5a3056
pudding <gyh020815@outlook.com>
2025-01-19 17:28:13 +0800

    add:1.19 不同二叉搜索树 动态规划和 卡特兰数 c=c*2(2*n+1)/n+2

--- src/main/java/org/tree/midium/NumTrees.java
+++ src/main/java/org/tree/midium/NumTrees.java
@@ -6,7 +6,34 @@ package org.tree.midium;
  * @Date 2025/1/19 下午4:50
  */
 public class NumTrees {
+    /*动态规划*/
+    /*dp[i] = i个不同的数组成的二叉搜索数的个数
+        假设 i = 5
+        当根节点等于 1 时 ，其余数字都比1大，只能在右边 dp[i] += dp[4]
+        当根节点等于 2 时，左边有一个1比2小，右边有三个比2大的数字 dp[i] += dp[1] * dp[3]
+        当根节点等于 3 时，左边有两个数比3小，右边有两个数比3大的数字 dp[i] += dp[2] * dp[2]
+        ...
+        知道根节点等于5，左边有4个数字比5小，只能放在5的左边,dp[i] += dp[4]*/
     public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
 
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+    /*数学 卡特兰数*/
+    public int numTrees1(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
     }
 }
--- src/test/java/Test05.java
+++ src/test/java/Test05.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.Random;
+import java.util.function.BiFunction;
 
 /**
  * @Author pudding
@@ -12,5 +14,14 @@ public class Test05 {
     public  void test01(){
         System.out.println("SD@!.;dwadw".toLowerCase());
         System.out.println(Character.isLetter('!'));
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("efsef",32);
+        Integer efsef = map.merge("efsef", 2332, Integer::sum);
+        System.out.println(efsef);
+        System.out.println(map);
+        System.out.println("==================================");
+        /*加减乘除的优先级高于左右位移*/
+        System.out.println(7+1<<2);
     }
 }
