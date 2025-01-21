cef7677f324585331180ddd922dd1342227b6f22
pudding <gyh020815@outlook.com>
2025-01-21 21:55:39 +0800

    add:1.21 银行中的激光束数量 2125模拟双循环 优化continue

--- src/main/java/org/array/secondary/NumberOfBeams.java
+++ src/main/java/org/array/secondary/NumberOfBeams.java
@@ -28,6 +28,28 @@ public class NumberOfBeams {
         return  res;
     }
 
+    /*优化contine*/
+    public int numberOfBeams1(String[] bank) {
+        int col = bank[0].length(),row = bank.length,res=0;
+        int preSum=0,curSum=0;
+        for (int i = 0; i < row; i++) {
+            char[] curRow = bank[i].toCharArray();
+            for (int j = 0; j < col; j++) {
+                if (curRow[j]=='1'){
+                    curSum++;
+                }
+            }
+            if (curSum >0) {
+                int sums = preSum * curSum;
+                preSum=curSum;
+                curSum=0;
+                res+=sums;
+            }
+
+        }
+        return  res;
+    }
+
     public static void main(String[] args) {
         NumberOfBeams numberOfBeams = new NumberOfBeams();
         System.out.println(numberOfBeams.numberOfBeams(new String[]{"011001","000000","010100","001000"}));
