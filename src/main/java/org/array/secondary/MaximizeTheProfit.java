c4fad403be8eae351a5c2b2926664fd747e2fd95
pudding <gyh020815@outlook.com>
2025-01-26 20:59:29 +0800

    add:1.26 40 组合总和Ⅱ

--- src/main/java/org/array/secondary/MaximizeTheProfit.java
+++ src/main/java/org/array/secondary/MaximizeTheProfit.java
@@ -1,9 +1,6 @@
 package org.array.secondary;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author pudding
@@ -33,6 +30,33 @@ public class MaximizeTheProfit {
             }
         }
         return low;
+    }
+
+    /*简便方法*/
+    public int maximizeTheProfit1(int n, List<List<Integer>> offers) {
+        //dp[i]表示在当前订单下，出售前i间房时能卖出的最多金币
+        int[] dp = new int[n+1];
+        Map<Integer,List<int[]>> groups = new HashMap<>();
+
+        //选或不选 => (选的有多个)选哪个
+        //按照end分组，因为该房子卖的话，可能有多种方案涉及到，要选最大价值的那套方案
+        for(List<Integer> offer: offers){
+            List<int[]> group = groups.getOrDefault(offer.get(1),new ArrayList<>());
+            group.add(new int[]{offer.get(0),offer.get(2)});
+            groups.put(offer.get(1),group);
+        }
+
+        for(int end=0;end<n;end++){//不管有没有买end号房的，直接从0遍历到n
+            List<int[]> group = groups.getOrDefault(end,new ArrayList<>());
+            //既有不选的作用，又能让dp保持连续
+            dp[end+1] = dp[end];
+            //选,买end号房的方案可能有多个
+            for(int[] endOffer:group){
+                dp[end+1] = Math.max(dp[end+1],dp[endOffer[0]] + endOffer[1]);
+            }
+        }
+
+        return dp[n];
     }
 
 }
