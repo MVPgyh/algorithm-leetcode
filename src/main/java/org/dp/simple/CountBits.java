103d7a2380718ea3afa7569585e2bbf22fdb6cbf
pudding <gyh020815@outlook.com>
2025-02-05 22:38:55 +0800

    add:2.5 子集2

--- src/main/java/org/dp/simple/CountBits.java
+++ src/main/java/org/dp/simple/CountBits.java
@@ -6,6 +6,7 @@ package org.dp.simple;
  * @Date 2025/2/4 下午10:59
  */
 public class CountBits {
+    /*动态规划*/
     public static int[] countBits(int n) {
         int[] bits = new int[n + 1];
         int highBit = 0;
@@ -17,6 +18,28 @@ public class CountBits {
         }
         return bits;
     }
+
+
+    /*Brian Kernighan算法 */
+    public int[] countBits1(int n) {
+        int[] bits = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            bits[i] = countOnes(i);
+        }
+        return bits;
+    }
+
+    public int countOnes(int x) {
+        int ones = 0;
+        while (x > 0) {
+            /*把x的比特位最右端的1变为0的结果*/
+            x &= (x - 1);
+            ones++;
+        }
+        return ones;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println(countBits(20));
